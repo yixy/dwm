e2f58de0ab59398d6f0d6df8ebc1e6725c38d7f6
youzhilane <youzhilane01@gmail.com>
2023-01-23 12:46:06 +0800

    alpha-patch
    Signed-off-by: youzhilane <youzhilane01@gmail.com>

--- config.h
+++ config.h
@@ -6,17 +6,24 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Noto Sans Mono:size=16" };
-static const char dmenufont[]       = "Noto Sans Mono:size=16";
+static const char *fonts[]          = { "Noto Sans Mono:size=8" };
+static const char dmenufont[]       = "Noto Sans Mono:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
