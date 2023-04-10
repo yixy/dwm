40f134fc3bd44860501cbd3b49b42b8266535aa4
yixy <youzhilane01@gmail.com>
2023-04-10 21:28:01 +0800

    change fontsize
    Signed-off-by: yixy <youzhilane01@gmail.com>

--- config.h
+++ config.h
@@ -7,10 +7,9 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 //static const char *fonts[]          = { "Noto Sans Mono:size=8" };
-static const char *fonts[]          = { "Noto Sans Mono:size=8",
-                "WenQuanYi Micro Hei:size=12:type=Regular:antialias=true:autohint=true",
-                "Symbols Nerd Font:pixelsize=32:type=2048-em:antialias=true:autohint=true"};
-static const char dmenufont[]       = "Noto Sans Mono:size=8";
+static const char *fonts[]          = { "Noto Sans Mono:size=12",
+                "Symbols Nerd Font Mono:size=12"};
+static const char dmenufont[]       = "Noto Sans Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -30,7 +29,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1 ", "2 ", "3 ", "4 ", "5 󰘅", "6 ", "7 ", "8 ", "9 " };
+static const char *tags[] = { "1", "2", "3", "4", "5󰘅", "6 ", "7 ", "8 ", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -71,7 +70,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "60*17", NULL };
 static const char *slockcmd[] = { "slock", NULL };
 //static const char *lightup[] = { "~/.scripts/lightup.sh", NULL };
 //static const char *lightdown[] = { "~/.scripts/lightdown.sh", NULL };
